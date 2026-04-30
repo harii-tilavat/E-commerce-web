bae997be698cabcdf59117fc1920535bb20b1468
harii-tilavat <harittilavat10@gmail.com>
2026-04-30 14:22:17 +0530

    add sinon setup

--- test/middlewares/auth.middleware.spec.js
+++ test/middlewares/auth.middleware.spec.js
@@ -1,4 +1,6 @@
 const { expect } = require('chai');
+const sinon = require('sinon');
+
 const { StatusCode } = require('../../utils/api-response');
 const JwtHelperService = require('../../services/jwt-helper.service');
 const requireAuth = require('../../middlewares/auth.middleware');
@@ -26,8 +28,28 @@ describe('Auth middleware', () => {
     });
   });
 
-  it('should not throw an error if token is valid and set user in request', () => {
+  it('should not throw an error if token is valid using sinon stub', () => {
     const payload = { id: '123', email: 'john@gmail.com' };
+    const token = 'valid-token';
+    const stub = sinon.stub(JwtHelperService, 'verifyToken').returns(payload);
+    const req = {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    };
+    requireAuth(req, {}, (err) => {
+      expect(err).to.equals(undefined);
+    });
+    expect(req.user.id).to.equals(payload.id);
+    expect(req.user.email).to.equals(payload.email);
+    expect(JwtHelperService.verifyToken.called).to.be.true;
+
+    // Restore the stub after each test
+    stub.restore();
+  });
+
+  it('should not throw an error if token is valid and set user in request', () => {
+    const payload = { id: '123', email: 'john2@email.com' };
     const token = JwtHelperService.generateToken(payload);
     const req = {
       headers: {
